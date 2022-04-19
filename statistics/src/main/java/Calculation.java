760d24ce130471b3ed0aa604a54d8ffac7e2bacc
Duan Yiting <wouktb@outlook.com>
2022-04-19 20:03:34 +0800

    multiple jobs for humidity

--- statistics/src/main/java/Calculation.java
+++ statistics/src/main/java/Calculation.java
@@ -23,6 +23,8 @@ public class Calculation {
     public static void main(String[] args) throws Exception {
         String maxTemperatureOutput = "output/maxTemperature";
         String minTemperatureOutput = "output/minTemperature";
+        String maxHumidityOutput = "output/maxHumidity";
+        String minHumidityOutput = "output/minHumidity";
 
         /* Max Temperature */
         Configuration conf1 = new Configuration();
@@ -51,6 +53,34 @@ public class Calculation {
         FileOutputFormat.setOutputPath(job2, new Path(minTemperatureOutput));
 
         job2.waitForCompletion(true);
+
+        /* Max Humidity */
+        Configuration conf3 = new Configuration();
+        Job job3 = Job.getInstance(conf3, "MaxHumidity");
+        job3.setJarByClass(Calculation.class);
+        job3.setMapperClass(HumidityMapper.class);
+        job3.setCombinerClass(FloatMaxReducer.class);
+        job3.setReducerClass(FloatMaxReducer.class);
+        job3.setOutputKeyClass(Text.class);
+        job3.setOutputValueClass(FloatWritable.class);
+        FileInputFormat.addInputPath(job3, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job3, new Path(maxHumidityOutput));
+
+        job3.waitForCompletion(true);
+
+        /* Min Humidity */
+        Configuration conf4 = new Configuration();
+        Job job4 = Job.getInstance(conf4, "MinHumidity");
+        job4.setJarByClass(Calculation.class);
+        job4.setMapperClass(HumidityMapper.class);
+        job4.setCombinerClass(FloatMinReducer.class);
+        job4.setReducerClass(FloatMinReducer.class);
+        job4.setOutputKeyClass(Text.class);
+        job4.setOutputValueClass(FloatWritable.class);
+        FileInputFormat.addInputPath(job4, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job4, new Path(minHumidityOutput));
+
+        job4.waitForCompletion(true);
     }
 
     public static class TemperatureMapper
@@ -70,7 +100,38 @@ public class Calculation {
                 String temperature = tokens[TEMPERATURE_C];
 
                 stationMonth.set(station + month);
-                floatValue.set(Float.parseFloat(temperature));
+                if (!temperature.equals("M")) {
+                    floatValue.set(Float.parseFloat(temperature));
+                } else {
+                    floatValue.set(Float.NaN);
+                }
+                context.write(stationMonth, floatValue);
+            }
+        }
+    }
+
+    public static class HumidityMapper
+            extends Mapper<Object, Text, Text, FloatWritable> {
+
+        private final FloatWritable floatValue = new FloatWritable();
+        private final Text stationMonth = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String record = value.toString();
+            if (!record.equals("station,valid,elevation,tmpc,relh,drct,sped")) {
+                String[] tokens = record.split(",");
+                String station = tokens[STATION];
+                String date = tokens[DATE];
+                String month = InputUtils.getMonth(date);
+                String humidity = tokens[HUMIDITY];
+
+                stationMonth.set(station + month);
+                if (!humidity.equals("M")) {
+                    floatValue.set(Float.parseFloat(humidity));
+                } else {
+                    floatValue.set(Float.NaN);
+                }
                 context.write(stationMonth, floatValue);
             }
         }
@@ -97,7 +158,7 @@ public class Calculation {
                 throws IOException, InterruptedException {
             float min = Float.MAX_VALUE;
             for (FloatWritable val : values) {
-                min = Float.max(min, val.get());
+                min = Float.min(min, val.get());
             }
             result.set(min);
             context.write(key, result);
