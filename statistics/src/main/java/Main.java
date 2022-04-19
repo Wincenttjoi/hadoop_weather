f84b1c52fe00f32678ca786c8dba338d89c3ed00
Duan Yiting <wouktb@outlook.com>
2022-04-19 14:00:27 +0800

    config

--- statistics/src/main/java/Main.java
+++ statistics/src/main/java/Main.java
@@ -1,3 +1,8 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, Duan Lao Shi!");
