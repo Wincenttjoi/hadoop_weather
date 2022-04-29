dd4db0e7d8a9acf5733efa9bd38b3dde5d16ab13
Dou Maokang <alecdou0001@gmail.com>
2022-04-30 02:48:34 +0800

    kmeans input

---
+++ statistics/hadoop-run-kmeans-input.sh
@@ -0,0 +1,3 @@
+
+# start mapreduce job
+hadoop jar ../statistics.jar KMeansInput output-stats/part-r-00000 output-kmeans
--- statistics/hadoop-run.sh
+++ statistics/hadoop-run.sh
@@ -6,7 +6,7 @@ hdfs dfs -mkdir input-stats
 hdfs dfs -put ../download/resources/raw_data/* input-stats
 
 # start mapreduce job
-hadoop jar ../statistics.jar Main input-stats output-stats
+hadoop jar ../statistics.jar KMeansInput input-stats output-stats
 
 # display output
 hdfs dfs -cat output-stats/part-r-00000
